51d0f0e54d3652bf095b20ddfe2c2e033bb96391
adithyasai20 <lalithsai20@iitk.ac.in>
2025-11-16 02:29:11 +0530

    FINAL COMMIT DONT TOUCH THIS PROJECT EVER!!!!!
    Signed-off-by: adithyasai20 <lalithsai20@iitk.ac.in>

---
+++ params.py
@@ -0,0 +1,6 @@
+image_size = 14
+data_path = '../../data/reservoir_yasir_v2.xlsx'
+EPOCHS = 10
+column_name = 'drain current'
+model_class = 'ResourceRichReadoutLayer'
+output_storage_path = '../../data/automated_code_output/'
